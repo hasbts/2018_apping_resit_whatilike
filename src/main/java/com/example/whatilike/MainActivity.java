2640a64b7a314a52e71d742fe584daa48dec2e96
hasbts <40206987+hasbts@users.noreply.github.com>
2019-01-22 16:50:41 +0100

    Add files via upload

--- src/main/java/com/example/whatilike/MainActivity.java
+++ src/main/java/com/example/whatilike/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.whatilike;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+    public Button but1;
+
+    public void init(){
+        but1 = (Button)findViewById(R.id.button_00);
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(MainActivity.this, MainActivity1.class);
+                startActivity(toy);
+            }
+        });
+    }
+
 }
